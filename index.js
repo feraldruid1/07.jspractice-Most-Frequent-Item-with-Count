4ac9d776c062e9de06c5e4dc85f04ced63de9b35
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-02 10:29:44 -0800

    practice

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const arr = [3,"a","a","a",2,3,"a",3,"a",2,4,9,3];
 let count=0;
-for(let i=0; i<arr.length; i++){
     arr.forEach(v => v === arr[0] && count++)
-    arr.shift;
-}
 console.log(count);
