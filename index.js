b017dafc15f7c133eb9290853b82d68d4e3352c6
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-02 10:39:08 -0800

    practice

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const arr = [3,"a","a","a",2,3,"a",3,"a",2,4,9,3];
 let count=0;
-    arr.forEach(v => v === arr[0] && count++)
+for(let i=0; i<arr.length; i++){
+    if(arr[i]===arr[0]){
+        count++
+    }
+}
 console.log(count);
