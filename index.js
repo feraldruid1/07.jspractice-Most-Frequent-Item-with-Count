4e5fbeca0d0f64ed71e81060903951ed203dfab8
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-03 13:44:51 -0800

    practice

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const arr = [3,"a","a","a",2,3,"a",3,"a",2,4,9,3];
+const array=[];
 let count=0;
+let countA=0;
+for(let y=0; y<arr.length; y++){
+    count=0;
 for(let i=0; i<arr.length; i++){
-    if(arr[i]===arr[0]){
-        count++
+    if(arr[i]===arr[countA]){
+        count++;
     }
 }
-console.log(count);
+countA++;
+array.push(count);
+}
+console.log(array);
