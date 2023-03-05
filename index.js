64ee63c1227c7dd03c551ceb9628bacb66f731d3
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-05 11:53:49 -0800

    practice

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const arr = [3,"a","a","a",2,3,"a",3,"a",2,4,9,3];
 const array=[];
-const iterator = array.values();
-const index=array.indexOf(Math.max(...array));
 let count=0;
 let countA=0;
 function arrMode(ar){
-for(let y=0; y<ar; y++){
+for(let y=0; y<ar.length; y++){
     count=0;
-for(let i=0; i<ar; i++){
+for(let i=0; i<ar.length; i++){
     if(ar[i]===ar[countA]){
         count++;
     }
@@ -15,6 +13,7 @@ for(let i=0; i<ar; i++){
 countA++;
 array.push(count);
 }
-console.log(arr[index]+"-"+Math.max(...array)+"times")
+const index=array.indexOf(Math.max(...array));
+return console.log(ar[index]+"-"+Math.max(...array)+"times")
 }
 arrMode(arr);
